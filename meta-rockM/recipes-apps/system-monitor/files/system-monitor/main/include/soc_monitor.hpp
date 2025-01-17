db5d0234f76671ff6dbc00c0ec835d07c9c80f46
Alex Vd <keromvp@gmail.com>
2025-01-17 19:29:34 +0300

    add monitoring functionallity

---
+++ meta-rockM/recipes-apps/system-monitor/files/system-monitor/main/include/soc_monitor.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <cstdint>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+namespace hwmon
+{
+
+int64_t
+readTemp(const std::string& path)
+{
+  int64_t val;
+  std::ifstream ifs;
+  ifs.exceptions(std::ifstream::failbit | std::ifstream::badbit
+                 | std::ifstream::eofbit);
+
+  errno = 0;
+  if (!ifs.is_open())
+    ifs.open(path);
+  ifs.clear();
+  ifs.seekg(0);
+  ifs >> val;
+
+  return val;
+}
+
+void
+runSocMonitor(const std::string& path)
+{
+  for (;;) {
+    auto temp = static_cast<float>(readTemp(path));
+    std::cout << "temp is " << (temp / 1000.0) << "\n";
+    sleep(1);
+  }
+}
+};  // namespace hwmon
